48c63c718a08b0ef7595c537986a69e903aaf0d9
EuLeEr <eermolov@mail.ru>
2020-08-12 18:08:29 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+document.getElementById("app").innerHTML = `
+<h1>Hello Parcel!</h1>
+<div>
+  Look
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
+  for more info about Parcel.
+</div>
+`;
+interface User {
+  firstName : string
+  lastName: string
+}
+const user: User = { firstName: "Евгений", lastName: "Ермолов"};
+document.getElementById("app").innerHTML = 
+document.getElementById("app").innerHTML + `
+<div>
+Copyright ${user.firstName} ${user.lastName}
+</div>
+`;
